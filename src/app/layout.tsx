1999f74c1f6a70087a7e92f76012ae54fac5949c
Zerum <imsoug071@gmail.com>
2024-10-04 15:22:05 +0900

    chore : removed unnecessary codes

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "중고마켓",
+  description: "중고거래 플랫폼",
+};
+
+function HTMLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className={"antialiased"}>{children}</body>
+    </html>
+  );
+}
+
+export default HTMLayout;
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+function HomePage() {
+  return <div>중고마켓</div>;
+}
+
+export default HomePage;
